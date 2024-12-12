35c2891d9fdd717582c13681368eb12109aaf773
Pramanananam <162253182+Joginggg@users.noreply.github.com>
2024-12-13 00:37:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def main():
-    st.title("Kuis Pemula" page_icon=📚)
+    st.set_page_config(page_title="Kuis Pemula", page_icon="📚")
     st.markdown("Selamat Datang di Kuis Pemula! Pemula atau Pemain udah lama?")
     
     st.markdown("""
