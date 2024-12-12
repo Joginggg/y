1c866946a0e655dba13673265ff2f2b73a79b411
Pramanananam <162253182+Joginggg@users.noreply.github.com>
2024-12-13 00:32:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from main import main as main_page
 from quiz import run_quiz
 
-st.set_page_config(page_title="Kuis Pemula", page_icon="")
+st.set_page_config(page_title="Kuis Pemula", page_icon="📚")
 
 def run():
     if 'page' not in st.session_state:
